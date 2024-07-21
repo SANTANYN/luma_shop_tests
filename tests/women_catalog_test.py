0f03e42ec330edc885eeb905b8327a71145f69e5
Mikhail <>
2024-07-21 17:49:15 +0700

    fixes

--- tests/women_catalog_test.py
+++ tests/women_catalog_test.py
@@ -10,7 +10,6 @@ from base.base_test import BaseTest
 class TestWomenCatalog(BaseTest):
 
     @allure.severity("Critical")
-    @pytest.mark.smoke
     @allure.title("check that we can navigate to women catalog page, and its exactly main woman catalog page")
     def test_navigate_to_women_page(self):
         self.home_page.open_home_page()
@@ -20,7 +19,6 @@ class TestWomenCatalog(BaseTest):
         assert title.text == "Women"
 
     @allure.severity("Critical")
-    @pytest.mark.smoke
     @allure.title("check that in the hot sellers block product cards are present")
     def test_check_hot_sellers_items(self):
         self.home_page.open_home_page()
@@ -30,18 +28,16 @@ class TestWomenCatalog(BaseTest):
         assert sellers_list == 4
 
     @allure.severity("Critical")
-    @pytest.mark.smoke
     @allure.title("check that in the catalog page exactly 12 cards in catalog")
     def test_12_product_cards_on_page(self):
         self.home_page.open_home_page()
         self.home_page.is_opened()
-        self.women_catalog_page.get_women_store_tops_page_element()
+        self.women_catalog_page.open_women_tops_page()
 
         assert self.women_catalog_page.product_cards_are_present() > 0, "No product cards found on the jackets page"
         assert self.women_catalog_page.product_cards_are_present() == 12, "it is not a 12 product cards on the page"
 
     @allure.severity("Critical")
-    @pytest.mark.smoke
     @allure.title("check that in bottoms short catalog page product cards are present")
     def test_product_card_are_present_in_bottom_shorts(self):
         self.home_page.open_home_page()
